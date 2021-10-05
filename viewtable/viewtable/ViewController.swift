7e502b94beb5c15ca8a0346aef442261c7288115
خلود سلطان علي آل عبدالرحمن <kholodsultanalialabdulrhman@MacBook-Pro-alkhas-b-khlwd.local>
2021-10-05 18:26:05 +0300

    alert day

--- viewtable/viewtable/ViewController.swift
+++ viewtable/viewtable/ViewController.swift
@@ -26,31 +26,29 @@ class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSo
         }
         else
         {
-            cell.customecell.backgroundColor = UIColor.white
+            cell.customecell.backgroundColor = UIColor.orange
         }
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let emojis = emojis[indexPath.row]
+        
         let alretcontroller = UIAlertController(
             title: "welcom to my frist app 😎 ", message: "Hello World 🎉!", preferredStyle: UIAlertController.Style.alert
         )
         
         alretcontroller.addAction(
             UIAlertAction(
-                title:"Now👀! You Are Our Guset🥳",
+                title:"ok\(emojis) ",
                 style:UIAlertAction.Style.default,
                 handler:{ Action in
-                    print ("Hi🤩")
+                    print ("HI")
                 })
             )
         
             present(alretcontroller, animated: true, completion: nil)
     }
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
