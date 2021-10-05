b1c95ec88e784bb61c1ad697f00e9d6638862767
hanan asiri <kayan222276@yahoo.com>
2021-10-05 17:14:12 +0300

    lab8

--- lab7/lab7-emoji/lab7-emoji/ViewController.swift
+++ lab7/lab7-emoji/lab7-emoji/ViewController.swift
@@ -22,14 +22,12 @@ class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSo
         super.viewDidLoad()
         tableview.delegate = self
         tableview.dataSource = self
-        
     }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return emojis.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "123", for: indexPath) as! Hanancell
         
@@ -40,16 +38,24 @@ class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSo
         } else {
             cell.label1.backgroundColor = UIColor.red
         }
-        
         return cell
     }
-    // Do any additional setup after loading the view.
-}
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let emoje = emojis [indexPath.row]
+        let alertcontroller = UIAlertController(title: "warning!!😆"
+                            , message: "we haved hacked you \(emoje) "
+                            , preferredStyle: UIAlertController.Style.alert
+        )
+        alertcontroller.addAction(
+          UIAlertAction(title: "pay500", style: UIAlertAction.Style.default, handler: { Action in print("hahahah")
+           
+          })
+        )
+        self.present(alertcontroller, animated: true, completion: nil)
+    }
 
+    }
 class Hanancell : UITableViewCell {
-    
     @IBOutlet weak var label1: UILabel!
-    
-    
-    
 }
